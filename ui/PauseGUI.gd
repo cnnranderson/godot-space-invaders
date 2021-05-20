722c632337b7ac643656f2fc5db4e3ac2abdd4ac
Conner Anderson <cnnranderson@gmail.com>
2021-05-20 09:02:04 -0700

    Add debug quit to desktop shortcut key when paused

--- ui/PauseGUI.gd
+++ ui/PauseGUI.gd
@@ -3,6 +3,9 @@ extends Control
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		_pause(!get_tree().paused)
+	
+	if Input.is_action_pressed("debug_quit"):
+		get_tree().quit()
 
 func _pause(shouldPause):
 	get_tree().paused = shouldPause
