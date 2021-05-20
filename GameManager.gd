f1ac69b7d955365653a456c24a6ab72432e87559
Conner Anderson <cnnranderson@gmail.com>
2021-05-20 09:04:35 -0700

    Rewrite the god awful enemy movement mechanics
    Now they actually work!

--- GameManager.gd
+++ GameManager.gd
@@ -2,6 +2,8 @@ extends Node
 
 const Enemy := preload("res://entities/Enemy.tscn")
 
+signal game_won
+
 var enemy_grid = []
 var wave_width = 8
 var wave_height = 4
@@ -14,33 +16,39 @@ var delay_timer = .1
 var delay_move_countdown = delay_timer
 var all_rows_moved = true
 var last_row_moved = -1
+var enemy_count = 0
+var is_dropping = false
 
 func _ready():
-	for y in range(4):
-		enemy_grid.append([])
-		for x in range(8):
-			var enemy = Enemy.instance()
-			enemy.position = Vector2(150 + (50 * x), 180 - (40 * y))
-			enemy.dir = wave_dir
-			enemy.connect("enemy_died", $GameGUI, "_on_Enemy_enemy_died")
-			enemy.connect("enemy_died", self, "_on_GameManager_enemy_died")
-			enemy_grid[y].append(enemy)
-			$Enemies.add_child(enemy)
-			
-	#_spawn_enemy_row(50, 1)
-	#_spawn_enemy_row(100, -1)
-	#_spawn_enemy_row(150, 1)
-	#_spawn_enemy_row(250, -1)
+	$YouWin.visible = false
+	_add_enemies()
 
 func _process(delta):
 	if Input.is_action_pressed("debug_quit"):
 		get_tree().quit()
-		
+	
 	if Input.is_action_pressed("debug_restart"):
 		return get_tree().reload_current_scene()
-		
-	for y in range(wave_height):
-		if not delay_move and last_row_moved < y:
+	
+	# Process enemy movement
+	if delay_move and is_dropping:
+		delay_move_countdown -= delta
+		if delay_move_countdown <= 0:
+			delay_move_countdown = delay_timer
+			delay_move = false
+	elif is_dropping:
+		for x in range(wave_width):
+			var e = enemy_grid[last_row_moved][x]
+			if is_instance_valid(e):
+				e.position.y += 20
+		last_row_moved += 1
+		if last_row_moved >= wave_height:
+			is_dropping = false
+			wave_dir = -wave_dir
+		else:
+			delay_move = true
+	else:
+		for y in range(wave_height):
 			var max_left = limit_right
 			var max_right = limit_left
 			var should_drop = false
@@ -54,36 +62,33 @@ func _process(delta):
 			
 			should_drop = max_left < limit_left or max_right > limit_right
 			if should_drop:
-				delay_move = true
-				last_row_moved += 1
-				for x in range(wave_width):
-					var e = enemy_grid[y][x]
-					if is_instance_valid(e):
-						e.position.y += 40
+				is_dropping = true
+				last_row_moved = 0
 			else:
 				for x in range(wave_width):
 					var e = enemy_grid[y][x]
 					if is_instance_valid(e):
 						e.position.x += wave_speed * delta * wave_dir
-						
-	delay_move_countdown -= delta
-	if delay_move_countdown <= 0:
-		delay_move_countdown = delay_timer
-		delay_move = false
-	
-	if last_row_moved >= wave_height - 1:
-		last_row_moved = -1
-		all_rows_moved = true
-		wave_dir = -wave_dir
 
-func _spawn_enemy_row(y, dir):
-	for n in range(8):
-		var enemy = Enemy.instance()
-		enemy.position = Vector2(150 + (50 * n), y)
-		enemy.dir = dir
-		enemy.connect("enemy_died", $GameGUI, "_on_Enemy_enemy_died")
-		enemy.connect("enemy_died", self, "_on_Enemy_enemy_died")
-		$Enemies.add_child(enemy)
-		
+func _add_enemies():
+	# Setup enemies
+	for y in range(wave_height):
+		enemy_grid.append([])
+		for x in range(wave_width):
+			var enemy = Enemy.instance()
+			enemy.position = Vector2(150 + (50 * x), 180 - (40 * y))
+			enemy.dir = wave_dir
+			enemy.connect("enemy_died", $GameGUI, "_on_Enemy_enemy_died")
+			enemy.connect("enemy_died", self, "_on_GameManager_enemy_died")
+			enemy_grid[y].append(enemy)
+			$Enemies.add_child(enemy)
+			enemy_count += 1
+
 func _on_GameManager_enemy_died(value):
 	wave_speed += 2
+	enemy_count -= 1
+	if enemy_count <= 0:
+		emit_signal("game_won")
+
+func _on_Main_game_won():
+	$YouWin.visible = true
