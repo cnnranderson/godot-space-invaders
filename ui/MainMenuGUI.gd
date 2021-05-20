ac7b967d676eec85b46ec00c4491188cac9513a6
Conner Anderson <cnnranderson@gmail.com>
2021-05-20 09:02:21 -0700

    Tween added to the title

--- ui/MainMenuGUI.gd
+++ ui/MainMenuGUI.gd
@@ -1,7 +1,20 @@
 extends Control
 
+onready var title_tween = [$MarginContainer/GameName.rect_position.y, $MarginContainer/GameName.rect_position.y + 10]
+
+func _ready():
+	_tween_title()
+
+func _tween_title():
+	$Tween.interpolate_property($MarginContainer/GameName, "rect_position:y", title_tween[0], title_tween[1], 2, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
+
 func _on_PlayButton_button_up():
 	var _switch = get_tree().change_scene("res://scenes/GameScene.tscn")
 
 func _on_QuitButton_button_up():
 	get_tree().quit()
+
+func _on_Tween_tween_completed(object, key):
+	title_tween.invert()
+	_tween_title()
